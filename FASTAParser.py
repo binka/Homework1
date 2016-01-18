327e9ac6fcb5a838bc4eb38f789f8e8b909b0146
Lincoln Samelson <lincoln.samelson@colorado.edu>
2016-01-17 21:00:38 -0700

    Reads FASTA format and calculates basic GC content

--- FASTAParser.py
+++ FASTAParser.py
@@ -1,3 +1,34 @@
 __author__ = "Lincoln Samelson"
-__email__ = "rob@spot.colorado.edu"
+__email__ = "lincoln.samelson@colorado.edu"
 
+from decimal import *
+
+def read_fasta(fp):
+    name, seq = None, []
+    for line in fp:
+        line = line.rstrip()
+        if line.startswith(">"):
+            if name: yield (name, ''.join(seq))
+            name, seq = line, []
+        else:
+            seq.append(line)
+    if name: yield (name, ''.join(seq))
+
+with open('mixed_alphabet.fasta') as fp:
+    for name, seq in read_fasta(fp):
+        print(name, seq)
+        total = len(seq)
+        total = float(total)
+        C = seq.count("C")
+        C = float(C)
+        G = seq.count("G")
+        G = float(G)
+        A = seq.count("A")
+        A = float(A)
+        T = seq.count("T")
+        T = float(T)
+
+        gc_content = Decimal((C+G)/total)
+        print
+        print "%.3f percent GC " % (gc_content)
+        print
