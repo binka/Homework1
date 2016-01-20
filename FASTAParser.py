ca5e2189f69f5b04c7363aacd6d8d30532a85dcd
Lincoln Samelson <lincoln.samelson@colorado.edu>
2016-01-19 21:05:42 -0700

    Added functions to do small tasks.  Got a super basic kmer finder working

--- FASTAParser.py
+++ FASTAParser.py
@@ -14,21 +14,38 @@ def read_fasta(fp):
             seq.append(line)
     if name: yield (name, ''.join(seq))
 
+
+def calculate_gc(seq):
+    total = len(seq)
+    total = float(total)
+    C = seq.count("C")
+    C = float(C)
+    G = seq.count("G")
+    G = float(G)
+    A = seq.count("A")
+    A = float(A)
+    T = seq.count("T")
+    T = float(T)
+    gc_content = Decimal((C+G)/total)
+    print
+    print "%.3f percent GC " % (gc_content)
+    print
+
+def find_kmer(seq, kmer):
+    if seq.find(kmer) == -1:
+        print "No K-mer found here!"
+    else:
+        print "Found your K-mer!"
+        print
+
+
+
+
 with open('mixed_alphabet.fasta') as fp:
     for name, seq in read_fasta(fp):
         print(name, seq)
-        total = len(seq)
-        total = float(total)
-        C = seq.count("C")
-        C = float(C)
-        G = seq.count("G")
-        G = float(G)
-        A = seq.count("A")
-        A = float(A)
-        T = seq.count("T")
-        T = float(T)
+        calculate_gc(seq)
+        kmer = "AT"
+        find_kmer(seq, kmer)
 
-        gc_content = Decimal((C+G)/total)
-        print
-        print "%.3f percent GC " % (gc_content)
-        print
+
